c73941103a9d0592904e20a728f590a5bb3eb566
devahmad899 <130634376+devahmad899@users.noreply.github.com>
2023-10-17 11:10:22 +0500

    added showpassword function and UI fixes
    added show and hide password functionality to login form and there was  an issue in UI related to background image which is fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,16 @@
 import '../assets/css/styles.css';
 import React, { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate=useNavigate()
+  const navigate = useNavigate()
 
-    const base_url = process.env.REACT_APP_BASE_URL
-    // console.log(base_url)
-    const end_point = '/newLogin'
-    const fullUrl = base_url + end_point
+  const base_url = process.env.REACT_APP_BASE_URL
+  // console.log(base_url)
+  const end_point = '/newLogin'
+  const fullUrl = base_url + end_point
 
-    // console.log(fullUrl)
+  // console.log(fullUrl)
 
   // maintaining the state of form in component
   const [formData, setFormData] = useState({
@@ -18,13 +18,21 @@ const Login = () => {
     password: "",
   });
 
+  // show hide password function
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const passwordInputType = showPassword ? "text" : "password";
+
   // handling the changes in form feild
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  var token=null;
+  var token = null;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -45,11 +53,11 @@ const Login = () => {
         console.log(data);
         alert(data.message);
         setFormData({
-            email: "",
-            password: "",
-          });
-        token=data.data[0]['token'];
-        localStorage.setItem('token',token)
+          email: "",
+          password: "",
+        });
+        token = data.data[0]['token'];
+        localStorage.setItem('token', token)
         // console.log(token)
         navigate('/home');
       } else {
@@ -68,33 +76,41 @@ const Login = () => {
   // };
 
   return (
-    <div className="login">
-    <h2>Login</h2>
-    <form className="login-form" onSubmit={handleSubmit}>
-      <div className="textbox">
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <span className="material-symbols-outlined"> account_circle </span>
+    <div className="form-holder auth">
+      <div className="login">
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="textbox">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined"> account_circle </span>
+          </div>
+          <div className="textbox">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined"> lock </span>
+            <span
+              className="material-symbols-outlined password-toggle"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? "visibility" : "visibility_off"}
+            </span>
+          </div>
+          <button type="submit">LOGIN</button>
+          <Link to={`/forgot-pass/${token}`}>Forgot your credentials?</Link>
+        </form>
       </div>
-      <div className="textbox">
-        <input
-           type="password"
-           name="password"
-           placeholder="Password"
-           value={formData.password}
-           onChange={handleChange} 
-        />
-        <span className="material-symbols-outlined"> lock </span>
-      </div>
-      <button type="submit">LOGIN</button>
-      <Link to={`/forgot-pass/${token}`}>Forgot your credentials?</Link>
-    </form>
-  </div>
+    </div>
   );
 };
 
