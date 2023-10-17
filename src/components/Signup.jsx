aaa1ecf11932ca281ad71655e8e669855ac90028
devahmad899 <130634376+devahmad899@users.noreply.github.com>
2023-10-17 17:31:36 +0500

    updated UI
    UI of signup page is update with password show hide function

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,19 +2,36 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const base_url = process.env.REACT_APP_BASE_URL
-    const end_point = '/signUp'
-    const fullUrl = base_url + end_point
+  const base_url = process.env.REACT_APP_BASE_URL
+  const end_point = '/signUp'
+  const fullUrl = base_url + end_point
 
   // maintaining the state of form in component
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
-    confirm_password:"",
+    confirm_password: "",
   });
+
+  // show hide password function
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordVisibility = (field) => {
+    if (field === "password") {
+      setShowPassword(!showPassword);
+    } else if (field === "confirm_password") {
+      setShowConfirmPassword(!showConfirmPassword);
+    }
+  };
+
+  const passwordInputType = showPassword ? "text" : "password";
+  const confirmPasswordInputType = showConfirmPassword ? "text" : "password";
+
+
 
   // handling the changes in form feild
   const handleChange = (e) => {
@@ -43,11 +60,11 @@ const Signup = () => {
         alert(data.message);
         navigate("/login");
         setFormData({
-            username: "",
-            email: "",
-            password: "",
-            confirm_password:"",
-          });
+          username: "",
+          email: "",
+          password: "",
+          confirm_password: "",
+        });
       } else {
         // Handle signup error
         console.error("Signup failed");
@@ -58,57 +75,65 @@ const Signup = () => {
   };
 
   return (
-    <div
-    style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        background: "skyblue",
-        alignItems: "center",
-        border: "1px solid black",
-        padding: "20px",
-        maxWidth: "35%", // Adjust this value to control the maximum width
-        margin: "10% auto", // Center the div horizontally within its parent
-      }}
-    >
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-        />{" "}
-        <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />{" "}
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />{" "}
-        <br />
-        <input
-          type="password"
-          name="confirm_password"
-          placeholder="Confirm Password"
-          value={formData.confirm_password}
-          onChange={handleChange}
-        />{" "}
-        <br />
-        <button type="submit">Sign Up</button> <br />
-        
-      </form>
-      <Link to={'/login'}>Already have account? Login</Link>
+    <div className="form-holder auth">
+      <div className="login">
+        <h2>Sing Up</h2>
+        <form className="login-form">
+          <div className="textbox">
+            <input type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined"> account_circle </span>
+          </div>
+          <div className="textbox">
+            <input type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined">mail</span>
+          </div>
+          <div className="textbox">
+            <input
+              type={passwordInputType}
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined"> lock </span>
+            <span
+              className="material-symbols-outlined password-toggle"
+              onClick={() => togglePasswordVisibility("password")}
+            >
+              {showPassword ? "visibility" : "visibility_off"}
+            </span>
+          </div>
+
+          <div className="textbox">
+            <input
+              type={confirmPasswordInputType}
+              name="confirm_password"
+              placeholder="Confirm Password"
+              value={formData.confirm_password}
+              onChange={handleChange}
+            />
+            <span className="material-symbols-outlined"> lock </span>
+            <span
+              className="material-symbols-outlined password-toggle"
+              onClick={() => togglePasswordVisibility("confirm_password")}
+            >
+              {showConfirmPassword ? "visibility" : "visibility_off"}
+            </span>
+          </div>
+          <button type="submit">Sing Up</button>
+          <Link to={'/login'}>Already have account? Login</Link>
+        </form>
+      </div>
     </div>
   );
 };
